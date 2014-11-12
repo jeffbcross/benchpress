f814dbd4c606da10b00e7e27613c9000a20d3392
Jeff Cross <middlefloor@gmail.com>
2014-11-11 16:20:59 -0800

    refactor(Runner): move runner into separate file

--- karma.conf.js
+++ karma.conf.js
@@ -15,6 +15,7 @@ module.exports = function(config) {
     // list of files / patterns to load in the browser
     files: [
       'lib/bp.js',
+      'lib/Runner.js',
       'lib/bp.spec.js',
       'node_modules/underscore/underscore.js'
     ],
